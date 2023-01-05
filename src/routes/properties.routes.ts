2805a4ceb86331aca7819b828b40cc5b0996ddb6
Leonardo Miranda do Nascimento <leonardo.miranda.3@hotmail.com>
2023-01-05 10:41:12 -0400

    Fix: call create property controller in properties routes

--- src/routes/properties.routes.ts
+++ src/routes/properties.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { createPropertyController } from '../controllers/property.controller';
 import { ensureAuthMiddleware } from '../middlewares/ensureAuth.middleware';
 import { ensureDataIsValidMiddleware } from '../middlewares/ensureDataIsValid.middleware';
 import { ensureIsAdmMiddleware } from '../middlewares/ensureIsAdm.middleware';
@@ -6,6 +7,6 @@ import { createPropertySerializer } from '../serializers/property.serializer';
 
 const propertyRoutes = Router();
 
-propertyRoutes.post('', ensureAuthMiddleware, ensureIsAdmMiddleware, ensureDataIsValidMiddleware(createPropertySerializer));
+propertyRoutes.post('', ensureAuthMiddleware, ensureIsAdmMiddleware, ensureDataIsValidMiddleware(createPropertySerializer), createPropertyController);
 
 export { propertyRoutes };
